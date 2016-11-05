56de87fd105a4cd9a27237dfd6c4a8172f591b8b
Ricky Valencia <3d.rickyV@gmail.com>
2016-11-05 13:48:20 -0700

    play with refs, play with multiple components

--- App.js
+++ App.js
@@ -4,34 +4,62 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { txt: 'this is the state text' };
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    };
     this.update = this.update.bind(this);
   }
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.input).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.input).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.input).value
+    });
   }
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        Red
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <hr />
+        Green
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <hr />
+        Blue
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
       </div>
-    )
+    );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type = "text" onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
-
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="input"
+               type = "range"
+               min="0"
+               max="255"
+               onChange={this.props.update} />
+      </div>
+    );
+  }
 }
+
+// const Widget = (props) => {
+//   return (
+//     <div>
+//       <input type = "text" onChange={props.update} />
+//       <h1>{props.txt}</h1>
+//     </div>
+//   );
+// };
+
 // App.propTypes = {
 //   txt: React.PropTypes.string,
 //   cat: React.PropTypes.number.isRequired
