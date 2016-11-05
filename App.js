e05201056921fbea188499adf1da77ab1f3418e5
Ricky Valencia <3d.rickyV@gmail.com>
2016-11-05 13:23:04 -0700

    practice props

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return (<div>hi</div>);
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>;
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number
+};
+// const App = () => <h1>Hello from stateless function component!</h1>;
+
+ReactDOM.render(
+  <App txt="this is a props text" />,
+  document.getElementById('app')
+);
 
 export default App;
