bf3e4f387d8ee6b184df0aa07626ffb2870c3cc0
Ricky Valencia <3d.rickyV@gmail.com>
2016-11-05 13:34:47 -0700

    play with parent to child components

--- App.js
+++ App.js
@@ -13,13 +13,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type = "text" onChange={this.update} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
-    );
+    )
   }
 }
 
+const Widget = (props) => {
+  return (
+    <div>
+      <input type = "text" onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+
+}
 // App.propTypes = {
 //   txt: React.PropTypes.string,
 //   cat: React.PropTypes.number.isRequired
