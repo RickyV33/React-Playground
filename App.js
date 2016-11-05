9a937dbb3e83b7fd6a61f894b668ccb89044a1a6
Ricky Valencia <3d.rickyV@gmail.com>
2016-11-05 13:30:05 -0700

    practice with components with state

--- App.js
+++ App.js
@@ -2,17 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { txt: 'this is the state text' };
+    this.update = this.update.bind(this);
+  }
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>;
+    return (
+      <div>
+        <input type = "text" onChange={this.update} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number
-};
-// const App = () => <h1>Hello from stateless function component!</h1>;
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// };
+//
+// App.defaultProps = {
+//   txt: 'this is a default value',
+//   cat: 100
+// };
+// const Stateless = () => <h1>Hello from stateless function component!</h1>;
 
 ReactDOM.render(
   <App txt="this is a props text" />,
