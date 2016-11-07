007a75cd8081fb093662dd7e67af39b450b57019
Ricky Valencia <3d.rickyV@gmail.com>
2016-11-06 21:55:57 -0800

    add really composable react API

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // ReactDOM.render(<App />, document.getElementById('app'));
-import App from './App4';
+import App from './App5';
